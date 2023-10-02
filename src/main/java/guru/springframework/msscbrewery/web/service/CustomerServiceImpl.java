0a9d5b62966a199621227f9aff6b70534cafcec2
nsoam <soam.neha@gmail.com>
2023-10-02 07:26:03 -0500

    Added Beer & customer controller

---
+++ src/main/java/guru/springframework/msscbrewery/web/service/CustomerServiceImpl.java
@@ -0,0 +1,18 @@
+package guru.springframework.msscbrewery.web.service;
+
+import guru.springframework.msscbrewery.web.model.CustomerDto;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+    @Override
+    public CustomerDto getCustomerById(UUID customerId) {
+        return CustomerDto.builder()
+                .id(UUID.randomUUID())
+                .customerName("Neha")
+                .build();
+    }
+}
